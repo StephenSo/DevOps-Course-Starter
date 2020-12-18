8bad84a8e659b44af3682ff3ffa44c6f8afbd889
Steve Spike <stephen.spike@gmail.com>
2020-12-18 11:54:54 +0000

    Step 3

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, session
+from flask import Flask, render_template, session, request
 
 from todo_app.flask_config import Config
 
@@ -19,9 +19,38 @@ def get_items():
     """
     return session.get('items', _DEFAULT_ITEMS)
 
-@app.route('/')
+def add_item(title):
+    """
+    Adds a new item with the specified title to the session.
+
+    Args:
+        title: The title of the item.
+
+    Returns:
+        item: The saved item.
+    """
+    items = get_items()
+
+    # Determine the ID for the item based on that of the previously added item
+    id = items[-1]['id'] + 1 if items else 0
+
+    item = { 'id': id, 'title': title, 'status': 'Not Started' }
+
+    # Add the item to the list
+    items.append(item)
+    session['items'] = items
+
+    return item
+
+@app.route('/',methods = ['POST', 'GET'])
 def index():
-    return render_template('index.html', items=get_items())
+    if request.method == 'POST':
+        result = request.form.get('newItem')
+        add_item(title=result)
+        return render_template('index.html', items=get_items())
+        
+    if request.method == 'GET':
+        return render_template('index.html', items=get_items())
 
 if __name__ == '__main__':
     app.run()
