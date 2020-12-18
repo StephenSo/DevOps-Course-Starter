a0073173178842e38a0f5b9979ebdcf5e02e3074
Steve Spike <stephen.spike@gmail.com>
2020-12-18 11:49:09 +0000

    Step 2.1

--- todo_app/app.py
+++ todo_app/app.py
@@ -21,7 +21,7 @@ def get_items():
 
 @app.route('/')
 def index():
-    return render_template('index.html', items=_DEFAULT_ITEMS)
+    return render_template('index.html', items=get_items())
 
 if __name__ == '__main__':
     app.run()
